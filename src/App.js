77a59087b89e76da79b4515ea7ac49f23c80ba13
revelationsz <68308751+revelationsz@users.noreply.github.com>
2021-08-03 15:22:36 -0400

    changed for kids use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <h1 className="header">
+        YOUR NAME'S website
+      </h1>
+
+      <p className="aboutME">
+        Paragraph about you, like age, grade ect
+      </p>
     </div>
   );
 }
